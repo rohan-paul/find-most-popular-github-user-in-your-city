29d9e93878ad3aeca73068b1502ae225be1f5c70
Rohan Paul <rohanpaul2@gmail.com>
2020-02-20 02:37:53 +0530

    small chnages in README.md

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ In the project directory, you can run:
 Runs the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-#### `yarn test`
+#### `yarn test` or `npm test`
 
 Launches the test runner in the interactive watch mode.<br />
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
--- src/reducers/__test__/userReducer.test.js
+++ src/reducers/__test__/userReducer.test.js
@@ -1,5 +1,13 @@
 import reducer from '../userReducer'
 import expect from 'expect'
+import * as actions from '../../actions/getUserActions'
+import {
+  LOADING,
+  LOAD_MOST_POPULAR_USERS,
+  ERROR_WHILE_FETCHING_INITIAL_TABLE,
+  CITY_TO_SEARCH,
+  SNACKBAR_STATUS,
+} from '../../actions/types'
 
 const initialState = {
   loading: false,
@@ -14,5 +22,33 @@ const initialState = {
 describe('post reducer', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('should handle SNACKBAR_STATUS', () => {
+    const snackbarAction = {
+      type: SNACKBAR_STATUS,
+      payload: true,
+    }
+    expect(reducer({}, snackbarAction)).toEqual({ snackbar: true })
+  })
+
+  it('should handle ERROR_WHILE_FETCHING_INITIAL_TABLE', () => {
+    const errorWhileFetchingTableAction = {
+      type: ERROR_WHILE_FETCHING_INITIAL_TABLE,
+      payload: true,
+    }
+    expect(reducer({}, errorWhileFetchingTableAction)).toEqual({
+      snackbar: true,
+    })
+  })
+
+  it('should handle CITY_TO_SEARCH', () => {
+    const cityToSearchAction = {
+      type: CITY_TO_SEARCH,
+      payload: 'Bengaluru',
+    }
+    expect(reducer({}, cityToSearchAction)).toEqual({
+      city_to_search: 'Bengaluru',
+    })
   })
 })
